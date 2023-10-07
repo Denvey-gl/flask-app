3ee3693bd9582e25b7efa765b22edeae0de76af6
Denvey-gl <83275492+Denvey-gl@users.noreply.github.com>
2023-10-07 16:00:35 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/tovar')
+def tovar():
+    return render_template('tovar.html')
+
+@app.route('/izbr')
+def izbr():
+    return render_template('izbr.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/friend')
+def friend():
+    return render_template('friend.html')
+
+@app.route('/tovar/<int:tovar_id>')
+def tovar_detail(tovar_id):
+    return render_template('tovar_detail.html',tovar_id=tovar_id)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
